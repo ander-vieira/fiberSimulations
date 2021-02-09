254b1e12bfb456789c42d3500c632da94a8404fb
Ander Vieira <ander.vieira741@gmail.com>
2021-02-09 17:50:06 +0100

    Documented energyTransferRate (FRET energy transfer calculation)

--- matlab/energyTransferRate.m
+++ matlab/energyTransferRate.m
@@ -1,14 +1,24 @@
 function rate = energyTransferRate(donorDopant, acceptorDopant, acceptorN)
-%ENERGYTRANSFERRATE Summary of this function goes here
-%   Detailed explanation goes here
+%ENERGYTRANSFERRATE Calculate FRET rate between two dye dopants
+%   Calculates the rate at which a dopant transfers its excited energy
+%   state to another, using the two dopants' luminescence spectra and the
+%   concentration of the acceptor dopant.
+%   See notes for the full expression given by Forster
+%
+%   Parameters:
+%   donorDopant: name of the donor DYE dopant
+%   acceptorDopant: name of the acceptor DYE dopant
+%   acceptorN: concentration of the acceptor dopant (m^-3)
 
 ll = (240:1:740)*1e-9;
 
-[tau, ~, donorEmiFun] = getDyeDopantAttributes(donorDopant);
+% Get the necessary parameters
+[tauRad, ~, donorEmiFun] = getDyeDopantAttributes(donorDopant);
 [~, acceptorAbsFun, ~] = getDyeDopantAttributes(acceptorDopant);
 
+% Measures the degree of alignment between the dopant molecules
+% A value of 2/3 is a good approximation for isotropic alignment
 kappa2 = 2/3;
-quantumYield = 1;
 
 donorEmi = donorEmiFun(ll);
 donorWNSP = donorEmi/sum(donorEmi);
@@ -16,9 +26,11 @@ acceptorAbs = acceptorAbsFun(ll);
 
 nPMMA = refractionIndexPMMA(ll);
 
+% Integral is done via discrete summation (could also use quadgk)
 integral = sum(donorWNSP.*acceptorAbs.*ll.^4./nPMMA.^4);
 
-rate = acceptorN^2*9*log(10)*kappa2*quantumYield*integral/(1280*pi^5)/tau;
+% The rate is obtained from the result of the integral (s^-1)
+rate = acceptorN^2*9*log(10)*kappa2*integral/(1280*pi^5)/tauRad;
 
 end
 
