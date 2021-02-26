0b4fc58a3e258abc1fad27ac6806ebce4aea81e1
Ander Vieira <ander.vieira741@gmail.com>
2021-02-26 23:26:10 +0100

    Cleanup of obsolete files

--- matlab/calculateBetaBasic.m
+++
@@ -1,9 +0,0 @@
-function beta = calculateBetaBasic(ll)
-%CALCULATEBETABASIC Summary of this function goes here
-%   Detailed explanation goes here
-
-ncore = refractionIndexPMMA(ll);
-
-beta = (ncore - 1)./(2*ncore);
-
-end
