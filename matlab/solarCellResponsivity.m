681953b307f2a4e0d846541ea4989f10be7897ca
Ander Vieira <ander.vieira741@gmail.com>
2021-02-28 20:17:50 +0100

    Fixed solarCellResponsivity using new readLambdaCsv syntax

--- matlab/solarCellResponsivity.m
+++ matlab/solarCellResponsivity.m
@@ -2,7 +2,9 @@ function solarCellR = solarCellResponsivity(ll)
 %SOLARCELLRESPONSIVITY Summary of this function goes here
 %   Detailed explanation goes here
 
-solarCellR = readLambdaCsv("../csv/solarCellResponsivity.csv", ll, 930.45e-9, 0.6478);
+solarCellFun = readLambdaCsv("../csv/solarCellResponsivity.csv", 930.45e-9, 0.6478);
+
+solarCellR = solarCellFun(ll);
 
 end
 
