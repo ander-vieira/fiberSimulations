03ca8f9e80f16b55af8531b239138c65e044c1ea
Ander Vieira <ander.vieira741@gmail.com>
2021-04-19 23:32:28 +0200

    Major overhaul to oneDopantRaytracing: added reflected rays + stimulated emission via recursive calls

--- matlab/oneDopantRaytracing.m
+++ matlab/oneDopantRaytracing.m
@@ -10,57 +10,68 @@ M = 1000000; % Number of photons to simulate
 
 rng('shuffle');
 
+% Constants
+c = 3e8; % m/s
+h = 6.63e-34; % J*s
+
+% Simulation parameters
+dz = 1e-4;
+zz = 0:dz:(lightL+darkL);
+numzz = length(zz);
+
 minlambda = 250e-9;
 dlambda = 2e-9;
 maxlambda = 750e-9;
+ll = minlambda:dlambda:maxlambda;
+
 da = 5e-5;
 
+% Rotation due to sunlight incidence angle
 incidenceAngle = deg2rad(incidenceAngleDegrees);
 rotationMatrix = [1 0 0 ; 0 cos(incidenceAngle) -sin(incidenceAngle) ; 0 sin(incidenceAngle) cos(incidenceAngle)]';
 
-[~, sigmaabsFun, sigmaemiFun] = getDyeDopantAttributes(dopant);
-
-ll = minlambda:dlambda:maxlambda;
+[tauRad, sigmaabsFun, sigmaemiFun, tauNR] = getDyeDopantAttributes(dopant);
 
 % Get probability distributions for sunlight photons and emitted photons
 solarDistribution = solarIrradianceSpline(ll);
 solarConstant = sum(solarDistribution);
 solarDistribution = solarDistribution/solarConstant;
 solarConstant = solarConstant*dlambda; % W/m^2
-emittedDistribution = sigmaemiFun(ll);
-emittedDistribution = emittedDistribution/sum(emittedDistribution);
 sigmaabsValues = sigmaabsFun(ll);
+sigmaemiValues = sigmaemiFun(ll);
+emittedDistribution = sigmaemiValues/sum(sigmaemiValues);
 alfaPMMAValues = attenuationPMMA(ll);
 
+quantumYield = tauNR/(tauRad+tauNR);
+% Conversion constant from power to N2
+conversionN2 = 4*ll/(pi*h*c*diameter^2*dz)/(1/tauRad+1/tauNR); % m^-3/W
+
 % Initial values
-position = zeros(2, 3);
+N2 = zeros(1, numzz);
 incomingPower = solarConstant*diameter*lightL*cos(incidenceAngle); % W
+minimumPower = incomingPower/M/100;
 finalPower = 0; % W
-finalWavelengths = zeros(1, M);
+totalPhotons = 0;
 finalPhotons = 0;
 runawayPhotons = 0;
 PMMAPhotons = 0;
-absorptions = zeros(1, M);
+stimulatedPhotons = 0;
 
-if nargout == 0
-    fprintf('i = 000000');
-end
-for i = 1:M
-    % Generate random photon from incident sunlight
-    k = generateDistributedLambda(ll, solarDistribution);
+% Run an individual photon through the fiber
+% Calls itself recursively for reflected photons and stimulated emission
+function runPhoton(position, direction, k, photonPower)
+    totalPhotons = totalPhotons + 1;
+    
     alfaPMMA = alfaPMMAValues(k);
     sigmaabs = sigmaabsValues(k);
-    photonPower = incomingPower/M;
+    sigmaemi = sigmaemiValues(k);
     
-    % Get initial position of photon (directly above fiber, random)
-    % and direction (rotated using incidence angle)
-    direction = [0 -1 0]*rotationMatrix;
-    position(2, :) = [diameter*(rand()-1/2) 0 lightL*rand()]+[0 0.55*diameter/rotationMatrix(2, 2) 0]*rotationMatrix;
+    position = [zeros(1, 3) ; position];
     
     loopOn = true;
     while loopOn
         position(1, :) = position(2, :);
-        
+
         % Get refraction index AND refraction gradient (for GI fibers)
         [prevInFiber, prevN, ~] = getRefractionIndex(position(1, :), diameter, ll(k));
         
@@ -71,7 +82,7 @@ for i = 1:M
         % only) (see notes)
 %         direction = direction + nGradient*da;
 %         direction = direction/vecnorm(direction);
-        
+
         % Calculate new position
         position(2, :) = position(1, :) + direction*ds;
         
@@ -123,6 +134,11 @@ for i = 1:M
                 photonPower = photonPower*fresnelT;
                 
                 % TODO: generate new photon with reflected part of power
+                reflectedDirection = 2*projectedVector - direction;
+                reflectedPower = photonPower*fresnelR;
+                if reflectedPower > minimumPower
+                    runPhoton(position(2, :)+reflectedDirection*prevN*da, reflectedDirection, k, reflectedPower);
+                end
                 
                 % Actually change the direction
                 direction = newDirection;
@@ -144,14 +160,35 @@ for i = 1:M
             end
         end
         
+        % Photon reaches right end of fiber (and is concentrated)
+        if(loopOn && inFiber && position(2, 3) >= lightL + darkL && position(1, 3) < lightL + darkL)
+            loopOn = false;
+            
+            finalPower = finalPower + photonPower;
+            finalPhotons = finalPhotons + 1;
+        end
+        
+        % Photon reaches ends of fiber but isn't concentrated (and is lost)
+        if(loopOn && inFiber && (position(2, 3) < 0 || position(2, 3) >= lightL + darkL))
+            loopOn = false;
+            
+            runawayPhotons = runawayPhotons + 1;
+        end
+        
         % Photon may be absorbed by PMMA (and lost)
         if(inFiber && rand() < alfaPMMA*ds)
             loopOn = false;
+            
             PMMAPhotons = PMMAPhotons + 1;
         end
         
+        % Index of the N2 interval to use
+        j = 1+floor((position(1, 3)+position(2, 3))/2/dz);
+        
         % Photon may be absorbed by dopant (and reemitted)
-        if(loopOn && inFiber && rand() < sigmaabs*N*ds)
+        if(loopOn && inFiber && rand() < sigmaabs*(N-N2(j))*ds)
+            N2(j) = N2(j)+photonPower*conversionN2(k);
+            
             % Generate random direction
             rand1 = 2*pi*rand();
             rand2 = 2*rand()-1;
@@ -162,30 +199,46 @@ for i = 1:M
             k = generateDistributedLambda(ll, emittedDistribution);
             alfaPMMA = alfaPMMAValues(k);
             sigmaabs = sigmaabsValues(k);
+            sigmaemi = sigmaemiValues(k);
             
             % Change photon power (Stokes shift loss)
             photonPower = photonPower*oldLambda/ll(k);
             
-            absorptions(i) = absorptions(i) + 1;
+            % Include non radiative decay chance
+            if rand() > quantumYield
+                loopOn = false;
+            end
         end
         
-        % Photon reaches right end of fiber (and is concentrated)
-        if(loopOn && inFiber && position(2, 3) >= lightL + darkL && position(1, 3) < lightL + darkL)
-            finalPower = finalPower + photonPower;
-            finalPhotons = finalPhotons + 1;
-            finalWavelengths(i) = ll(k);
-            loopOn = false;
-        end
-        
-        % Photon reaches ends of fiber but isn't concentrated (and is lost)
-        if(loopOn && inFiber && (position(2, 3) < 0 || position(2, 3) >= lightL + darkL))
-            loopOn = false;
+        % Photon may cause stimulated emission by the dopant
+        if(loopOn && inFiber && rand() < sigmaemi*N2(j)*ds)
+            N2(j) = N2(j) - photonPower*conversionN2(k);
+            stimulatedPhotons = stimulatedPhotons + 1;
             
-            runawayPhotons = runawayPhotons + 1;
+            runPhoton(position(2, :), direction, k, photonPower);
         end
     end
+end
+
+if nargout == 0
+    fprintf('i = 000000');
+end
+for i = 1:M
+    % Generate random photon from incident sunlight
+    k = generateDistributedLambda(ll, solarDistribution);
+%     alfaPMMA = alfaPMMAValues(k);
+%     sigmaabs = sigmaabsValues(k);
+    photonPower = incomingPower/M;
     
-    if nargout == 0 && mod(i, 1000) == 0
+    % Get initial position of photon (directly above fiber, random)
+    % and direction (rotated using incidence angle)
+    direction = [0 -1 0]*rotationMatrix;
+%     position(2, :) = [diameter*(rand()-1/2) 0 lightL*rand()]+[0 0.55*diameter/rotationMatrix(2, 2) 0]*rotationMatrix;
+      position = [diameter*(rand()-1/2) 0 lightL*rand()]+[0 0.55*diameter/rotationMatrix(2, 2) 0]*rotationMatrix;
+
+    runPhoton(position, direction, k, photonPower);
+    
+    if nargout == 0 && mod(i, 5000) == 0
         fprintf('\b\b\b\b\b\b%06d', i);
     end
 end
@@ -193,22 +246,13 @@ if nargout == 0
     fprintf('\n');
 end
 
-finalWavelengths = finalWavelengths(finalWavelengths>0);
-
 if nargout == 0
     fprintf('Simulation time: %.1f s\n', toc());
     fprintf('Output power of fiber: %g uW\n', finalPower*1e6);
-    fprintf('Photons reaching output: %d/%d\n', finalPhotons, M);
-    fprintf('Photons leaving the fiber: %d/%d\n', runawayPhotons, M);
-    fprintf('Photons absorbed by PMMA: %d/%d\n', PMMAPhotons, M);
-    fprintf('Average absorptions: %.4f\n', sum(absorptions)/M);
-    fprintf('Photons absorbed by the fiber: %d/%d\n', sum(absorptions > 0), M);
-    
-    figure(1);
-    histogram(finalWavelengths, 100);
-    
-    figure(2);
-    histogram(absorptions);
+    fprintf('Photons reaching output: %d/%d\n', finalPhotons, totalPhotons);
+    fprintf('Photons leaving the fiber: %d/%d\n', runawayPhotons, totalPhotons);
+    fprintf('Photons absorbed by PMMA: %d/%d\n', PMMAPhotons, totalPhotons);
+    fprintf('Stimulated emission photons: %d/%d\n', stimulatedPhotons, totalPhotons);
 end
 
 end
