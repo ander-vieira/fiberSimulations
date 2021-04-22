7bf5886b606d2a45e094900a6f20b9ac0b3248ac
Ander Vieira <ander.vieira741@gmail.com>
2021-04-22 18:09:27 +0200

    Fixed europium sigmas to new values

--- matlab/getEarthDopantAttributes.m
+++ matlab/getEarthDopantAttributes.m
@@ -5,15 +5,15 @@ if strcmp(dopant, 'AC56')
     tauD = 1.02e-3;
     wTD = 8e8;
     wDT = 2e8;
-    sigmaabs = readLambdaCsv("../csv/sigmaabs_AC56.csv", 379e-9, 6.9e-23);
-    sigmaemiD = readLambdaCsv("../csv/sigmaemi_AC56.csv", 614e-9, 4.64e-24);
+    sigmaabs = readLambdaCsv("../csv/sigmaabs_AC56.csv", 379e-9, 1e-20);
+    sigmaemiD = readLambdaCsv("../csv/sigmaemi_AC56.csv", 614e-9, 1e-21);
 else % Default is AC46
     tauT = 1e-5;
     tauD = 1.02e-3;
     wTD = 8e8;
     wDT = 2e8;
-    sigmaabs = readLambdaCsv("../csv/sigmaabs_AC46.csv", 379e-9, 3.45e-23);
-    sigmaemiD = readLambdaCsv("../csv/sigmaemi_AC46.csv", 614e-9, 4.64e-24);
+    sigmaabs = readLambdaCsv("../csv/sigmaabs_AC46.csv", 379e-9, 1e-20);
+    sigmaemiD = readLambdaCsv("../csv/sigmaemi_AC46.csv", 614e-9, 1e-21);
 end
 
 end
